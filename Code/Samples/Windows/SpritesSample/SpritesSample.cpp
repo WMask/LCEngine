3c545b0d016530c3cb8dfa4243b6bc60b1d179e7
Denis Romakhov <woodenmask@yandex.ru>
2023-08-27 17:47:09 +0400

    Added Sprites sample.

--- Code/Samples/Windows/SpritesSample/SpritesSample.cpp
+++ Code/Samples/Windows/SpritesSample/SpritesSample.cpp
@@ -1,11 +1,11 @@
 /**
-* HelloWorld.cpp
-* 28.01.2023
+* SpritesSample.cpp
+* 27.08.2023
 * (c) Denis Romakhov
 */
 
 #include "framework.h"
-#include "HelloWorld.h"
+#include "SpritesSample.h"
 #include "Application/AppConfig.h"
 #include "Application/Application.h"
 #include "Application/Windows/Module.h"
@@ -23,9 +23,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     {
         auto onInitHandler = [](IApplication* app)
         {
-            if (auto sprite = app->GetWorld()->AddSprite(250, 400, 200, 200))
+            if (auto sprite1 = app->GetWorld()->AddSprite(150, 400, 200, 200))
             {
-                sprite->AddColorsComponent(LcColor3(1, 0, 0), LcColor3(1, 0, 1), LcColor3(0, 0, 0), LcColor3(0, 1, 0));
+                sprite1->AddColorsComponent(LcColor3(1, 0, 0), LcColor3(1, 0, 1), LcColor3(0, 0, 0), LcColor3(0, 1, 0));
+            }
+
+            if (auto sprite2 = app->GetWorld()->AddSprite(150, 150, 200, 200))
+            {
+                sprite2->AddTintComponent(LcColor3(0.7f, 0.7f, 0.7f));
+            }
+
+            if (auto sprite3 = app->GetWorld()->AddSprite(450, 200, 300, 300))
+            {
+                sprite3->AddTextureComponent("tree.png");
+                sprite3->AddColorsComponent(LcColor3(0, 1, 0), LcColor3(0, 1, 0), LcColor3(1, 0, 0), LcColor3(1, 0, 0));
+            }
+
+            if (auto sprite4 = app->GetWorld()->AddSprite(380, 85, 100, 100))
+            {
+                sprite4->AddTextureComponent("anim.png");
+                sprite4->AddAnimationComponent(LcSizef(128, 128), 10, 12);
             }
         };
 
@@ -34,14 +51,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
         {
             DebugMsg("FPS: %.3f\n", (1.0f / deltaSeconds));
 
-            auto sprite = app->GetWorld()->GetSprites()[0];
+            auto sprite1 = app->GetWorld()->GetSprites()[0];
+            auto sprite2 = app->GetWorld()->GetSprites()[1];
 
             // move sprite
-            if (keys[VK_LEFT]) sprite->AddPos(LcVector3(-200 * deltaSeconds, 0, 0));
-            if (keys[VK_RIGHT]) sprite->AddPos(LcVector3(200 * deltaSeconds, 0, 0));
+            if (keys[VK_LEFT]) sprite1->AddPos(LcVector3(-200 * deltaSeconds, 0, 0));
+            if (keys[VK_RIGHT]) sprite1->AddPos(LcVector3(200 * deltaSeconds, 0, 0));
 
-            if (keys[VK_UP]) sprite->AddRotZ(-2 * deltaSeconds);
-            if (keys[VK_DOWN]) sprite->AddRotZ(2 * deltaSeconds);
+            if (keys[VK_UP]) sprite1->AddRotZ(-2 * deltaSeconds);
+            if (keys[VK_DOWN]) sprite1->AddRotZ(2 * deltaSeconds);
+
+            // change tint
+            auto value = sin(float(GetTickCount64()) / 1000.0f);
+            auto tint = float(abs(value));
+            sprite2->GetTintComponent()->SetColor(LcColor4(1.0f - tint, tint, 0.0f, 1.0f));
         };
 
         auto onKeyboardHandler = [&keys](int key, LcKeyState keyEvent, IApplication* app)
