c6ba35f14ac986f7e774bfe15b715dc0e3e5a8ec
Denis Romakhov <woodenmask@yandex.ru>
2023-09-26 08:11:50 +0400

    Fixed application config.

--- Code/Engine/Application/AppConfig.cpp
+++ Code/Engine/Application/AppConfig.cpp
@@ -8,12 +8,9 @@
 #include "Application/AppConfig.h"
 #include "Core/LCUtils.h"
 
-#include <iostream>
-#include <sstream>
+#include "Json/include/nlohmann/json.hpp"
+using json = nlohmann::json;
 
-#ifdef _WINDOWS
-# include <stdlib.h>
-#endif
 
 LCAppConfig::LCAppConfig()
 {
@@ -29,86 +26,26 @@ bool LoadConfig(LCAppConfig& outConfig, const char* fileName, char delim)
 	auto fullConfigText = ReadTextFile(fileName);
 	if (fullConfigText.length() < 3) return false;
 
-	auto isBool = [](const std::string& str) {
-		return str == "true" || str == "false";
-	};
+	json cfg = json::parse(fullConfigText);
 
-	auto isInt = [](const std::string& str) {
-		for (auto c : str)
-		{
-			if (c == '-') continue;
-			if (c < '0' || c > '9') return false;
-		}
-		return true;
-	};
-
-	auto isFloat = [](const std::string& str) {
-		bool dotFound = false;
-		for (auto c : str)
-		{
-			if (c == '-') continue;
-			if (c == '.') { dotFound = true; continue; }
-			if (c < '0' || c > '9') return false;
-		}
-		return dotFound;
-	};
-
-	std::string line;
-	std::stringstream cs(fullConfigText);
-	while (std::getline(cs, line, delim))
-	{
-		if (line.length() < 3) continue;
-
-		std::stringstream ls(line);
-		std::string key;
-		ls >> key;
-		if (key.empty()) continue;
-		if (key[0] == '#' || key[0] == '*' || key[0] == '/' || key[0] == '[') continue;
-
-		std::string value;
-		ls >> value;
-		if (value.empty()) continue;
-
-		LcAny item;
-
-		if (isBool(value)) item = LcAny(value == "true");
-#ifdef _WINDOWS
-		else
-		if (isFloat(value)) item = LcAny((float)atof(value.c_str()));
-		else
-		if (isInt(value)) item = LcAny(atoi(value.c_str()));
-#endif
-		else item = LcAny(value);
-
-		if (false)
-			return false;
-		else if (key == "WinWidth")
-			outConfig.WinWidth = item.iValue;
-		else if (key == "WinHeight")
-			outConfig.WinHeight = item.iValue;
-		else if (key == "bVSync")
-			outConfig.bVSync = item.bValue;
-		else if (key == "bAllowFullscreen")
-			outConfig.bVSync = item.bValue;
-		else if (key == "bNoDelay")
-			outConfig.bNoDelay = item.bValue;
-	}
+	outConfig.WinWidth			= cfg["WinWidth"].get<unsigned int>();
+	outConfig.WinHeight			= cfg["WinHeight"].get<unsigned int>();
+	outConfig.bVSync			= cfg["bVSync"].get<bool>();
+	outConfig.bAllowFullscreen	= cfg["bAllowFullscreen"].get<bool>();
+	outConfig.bNoDelay			= cfg["bNoDelay"].get<bool>();
 
 	return true;
 }
 
 void SaveConfig(const LCAppConfig& config, const char* fileName)
 {
-	std::stringstream output;
+	json cfg = {
+		{"WinWidth",			config.WinWidth},
+		{"WinHeight",			config.WinHeight},
+		{"bVSync",				config.bVSync},
+		{"bAllowFullscreen",	config.bAllowFullscreen},
+		{"bNoDelay",			config.bNoDelay},
+	};
 
-	output << "[Application]\n";
-	output << "WinWidth            " << config.WinWidth << '\n';
-	output << "WinHeight           " << config.WinHeight << '\n';
-
-	output << "\n[Render]\n";
-	output << "bVSync              " << (config.bVSync ? "true" : "false") << '\n';
-	output << "bAllowFullscreen    " << (config.bAllowFullscreen ? "true" : "false") << '\n';
-	output << "bNoDelay            " << (config.bNoDelay ? "true" : "false") << '\n';
-
-	WriteTextFile(fileName, output.str());
+	WriteTextFile(fileName, cfg.dump(4) + "\n");
 }
